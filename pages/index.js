884aae0c677cda483e7c7a263bf1fafaaace6151
samuelgarbo <samuel.garbo@gmail.com>
2021-04-14 08:41:43 +0300

    Remove apollo client

--- pages/index.js
+++ pages/index.js
@@ -8,18 +8,8 @@ import Footer from "./components/Footer";
 import CarRegister from "./components/CarRegister";
 import VehicleType from "./components/VehicleType";
 import CategoryNav from "./components/CategoryNav";
-import { __EnumValue } from "graphql";
 
-export default function Home(props) {
-  // let categories = [];
-
-  // data.collections.edges.forEach((edge) => {
-  //   let arr = edge.node.title.split(":");
-  //   if (arr.length === 1) {
-  //     categories.push(edge.node);
-  //   }
-  // });
-  // console.log(categories);
+export default function Home({ data }) {
   return (
     <div>
       <Header />
@@ -28,7 +18,7 @@ export default function Home(props) {
         <section tw="col-span-1">
           <CarRegister />
           <VehicleType />
-          <CategoryNav />
+          <CategoryNav data={data} />
         </section>
       </main>
       <Footer />
@@ -36,31 +26,57 @@ export default function Home(props) {
   );
 }
 
-// export async function getStaticProps(context) {
-//   let res = await fetch(
-//     "https://samuel-2643859.myshopify.com/api/2021-04/graphql.json",
-//     {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//         "X-Shopify-Storefront-Access-Token": process.env.SHOPIFY_SECRET,
-//       },
-//       body: JSON.stringify({
-//         query: `
-//       query{
-//         collections(first:10){
-//             edges{
-//                 node{
-//                     title
-//                 }
-//             }
-//         }
-//        }`,
-//       }),
-//     }
-//   );
-//   let json = await res.json();
-//   return {
-//     props: { data: json.data }, // will be passed to the page component as props
-//   };
-// }
+export async function getStaticProps(context) {
+  let res = await fetch(
+    "https://samuel-2643859.myshopify.com/api/2021-04/graphql.json",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-Shopify-Storefront-Access-Token": process.env.SHOPIFY_SECRET,
+      },
+      body: JSON.stringify({
+        query: `
+      query{
+        collections(first:10){
+            edges{
+                node{
+                    title
+                }
+            }
+        }
+       }`,
+      }),
+    }
+  );
+  let jsonData = await res.json();
+  let data = [];
+  jsonData.data.collections.edges.forEach((edge) => {
+    let arr = edge.node.title.split(":");
+    if (arr.length === 1) {
+      data.push({ id: arr[0].toLowerCase(), parent: "root" });
+    } else if (arr.length > 1) {
+      data.push({ id: arr[0].toLowerCase(), parent: arr[1].toLowerCase() });
+    }
+  });
+  data.unshift({ id: "root", parent: null });
+  const idMapping = await data.reduce((acc, el, i) => {
+    acc[el.id] = i;
+    return acc;
+  }, {});
+  let root;
+  data.forEach((el) => {
+    // Handle the root element
+    if (el.parent === null) {
+      root = el;
+      return;
+    }
+    // Use mapping to locate the parent element in data array
+    let parentEl = data[idMapping[el.parent]];
+    // Add current el to parent's children array
+    parentEl.children = [...(parentEl.children || []), el];
+  });
+  return {
+    props: { data: root }, // will be passed to the page component as props
+  };
+}
